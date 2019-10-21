1c518977de5d7cc51e9eaeb2eef765c997646ef9
imvamsi <krrishvamsi7@gmail.com>
2019-10-21 18:32:11 -0400

    added database connection to the application :pencil:

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.mongoURI, { useNewUrlParser: true })
+  .then(() => console.log("database connected"))
+  .catch(err => console.log(err));
 
 const server = new ApolloServer({
   typeDefs,
