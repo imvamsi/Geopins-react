c18c96eb96620436c38831102081461a9f15d729
imvamsi <krrishvamsi7@gmail.com>
2019-10-21 19:52:04 -0400

    added OAuth setup and google login button

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { GoogleLogin } from "react-google-login";
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const onSuccess = user => {
+    const idToken = user.getAuthResponse().id_token;
+    console.log({ idToken });
+  };
+  return (
+    <GoogleLogin
+      clientId="699892671462-ck209aolskfjsm0ci2bb50f16cjljkot.apps.googleusercontent.com"
+      onSuccess={onSuccess}
+      isSignedIn={true}
+    />
+  );
 };
 
 const styles = {
