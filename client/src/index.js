4a0cae890307b7f176c78776c2a37cef2e9bf936
imvamsi <krrishvamsi7@gmail.com>
2019-10-21 19:14:18 -0400

    added project structure :bookmark:

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./pages/App";
+import Splash from "./pages/Splash";
+
+import "mapbox-gl/dist/mapbox-gl.css";
+import * as serviceWorker from "./serviceWorker";
+
+const Root = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Splash} />
+      </Switch>
+    </Router>
+  );
+};
+
+ReactDOM.render(<Root />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
